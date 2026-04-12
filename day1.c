621592f2d3b4313eb00d7b65dbe6b3b78dac6371
Melissa Marcaida <melmarcaida1@gmail.com>
2026-04-12 13:10:40 +0800

    day3

--- day1.c
+++ day1.c
@@ -3,19 +3,20 @@
 #include <ctype.h>
 
 // case-sensitive function 
-int toLowerCase(char mssg[]) {
+void toLowerCase(char mssg[]) {
     for (int i = 0; mssg[i] != '\0'; i++) {
         mssg[i] = tolower(mssg[i]);
     }
-    return 0;
 }
 
 int main(void) {
 
     char mssg[200];
     char *keywords[] = {"free", "win", "money", "offer", "click"};
-    int weights[] = {3, 2, 4, 2, 1};
-    int score = 0;
+    double spamWeight[] = {0.8, 0.7, 0.9, 0.6, 0.65};
+    double notSpamWeight[] = {0.2, 0.3, 0.1, 0.4, 0.35};
+    double spamProb = 0.5;
+    double notSpamProb = 0.5;
     int found = 0; // not found
 
     printf("Enter a message btch: ");
@@ -23,16 +24,23 @@ int main(void) {
     mssg[strcspn(mssg, "\n")] = 0;
 
     toLowerCase(mssg);
+
     for (int i = 0; i < 5; i++) {
         if(strstr(mssg, keywords[i]) != NULL) { //keyword[i] here represents the whole word
-          score += weights[i];
+          spamProb *= spamWeight[i];
+          notSpamProb *= notSpamWeight[i];
           found = 1;
-          printf("Found: %s (%d)\n", keywords[i], weights[i]);
+          printf("Found: %s \n", keywords[i]);
        }
     }
+    printf("Spam Prob: %.5f\n", spamProb);
+    printf("Not Spam Prob: %.5f\n", notSpamProb);
+    
+    if (!found) { //f no keyw is found, the if will nver run thus found stays 0
+        printf("no keywords found hehsz \n");
+    }
 
-    printf("Score: %d\n", score); 
-    if (score >= 5) {
+    if (spamProb > notSpamProb) {
         printf("this message is a spam btch.\n");
     } else {
         printf("u safe. not a spam hehez.");
