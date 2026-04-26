64a22c578ff16a45947897682e49e3f13813f0be
melrcai <melmarcaida1@gmail.com>
2026-04-26 23:19:18 +0800

    pointers

---
+++ idk/num1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int A[5] = {10, 20, 30, 40, 50};
+    int *start = A;
+    int *end = &A[4];
+
+    while (start < end) {
+      int temp = *start;
+      *start = *end;
+      *end = temp;
+      start++;
+      end--;
+    }
+
+    printf("REVERSED: ");
+    for (int i = 0; i < 5; i++) {
+      printf("%d, ", A[i]);
+    }
+    
+    return 0;
+}
